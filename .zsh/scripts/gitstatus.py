66c17266cde2da0ada96825fc58a7b07d164a808
Sam Lea <samjlea@gmail.com>
2014-06-12 21:35:09 +1200

    Create the complete git prompt string in python script
    Remove string versions of number of changed/staged/conflicting/untracked files. No longer required.

--- .zsh/scripts/gitstatus.py
+++ .zsh/scripts/gitstatus.py
@@ -2,11 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 from __future__ import print_function
+from common import *
 
 # change those symbols to whatever you prefer
 #symbols = {'ahead of': '↑', 'behind': '↓', 'prehash':':'}
-#symbols = {'ahead of': '⬆', 'behind': '⬇', 'prehash':':'}
-symbols = {'ahead of': '▲', 'behind': '▼', 'prehash':':', 'diverged': '⭠'}
+symbols = {'ahead of': '⬆', 'behind': '⬇', 'prehash':':', 'diverged': '⭠'}
+#symbols = {'ahead of': '▲', 'behind': '▼', 'prehash':':', 'diverged': '⭠'}
 
 from subprocess import Popen, PIPE
 
@@ -35,34 +36,33 @@ staged_files = [namestat[0] for namestat in Popen(['git','diff', '--staged','--n
 # status 'U' is unmerged files
 # number of modified files 'M' status = all results - 'U' status files
 nb_changed = len(changed_files) - changed_files.count('U')
-changed = str(nb_changed)
 
 # conflicts/unmerged files
 nb_U = staged_files.count('U') # conflicts
-conflicts = str(nb_U)
 
 # number of staged files = all results - 'U' status files
 nb_staged = len(staged_files) - nb_U
-staged = str(nb_staged)
 
 # untracked files
 nb_untracked = len(Popen(['git','ls-files','--others','--exclude-standard'],stdout=PIPE).communicate()[0].splitlines())
-untracked = str(nb_untracked)
 
 # TODO add git stash list
 
 # check if the repo is clean
 # i.e. no changed, staged, unmerged, untracked files
 if not nb_changed and not nb_staged and not nb_U and not nb_untracked:
-    clean = '1'
+    clean = True
 else:
-    clean = '0'
+    clean = False
 
 remote = ''
+detached = False
+tracked = False
 if not branch: # not on any branch
     # red
     #print('no branch')
     branch = symbols['prehash']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0][:-1]
+    detached = True
 else: # on either a remote (tracked) or local only branch
     remote_name = Popen(['git','config','branch.%s.remote' % branch], stdout=PIPE).communicate()[0].strip()
     if remote_name:
@@ -73,6 +73,7 @@ else: # on either a remote (tracked) or local only branch
         else:
             #print('remote branch')
             remote_ref = 'refs/remotes/%s/%s' % (remote_name, merge_name[11:])
+            tracked = True
         revgit = Popen(['git', 'rev-list', '--left-right', '%s...HEAD' % remote_ref],stdout=PIPE, stderr=PIPE)
         revlist = revgit.communicate()[0]
         if revgit.poll(): # fallback to local
@@ -85,23 +86,48 @@ else: # on either a remote (tracked) or local only branch
         behind = len(behead) - ahead
         #print (behind)
         if behind != 0 and ahead != 0:
-            #print('foo')
-            remote += '%s%s%s' % (behind, symbols['diverged'], ahead)
+            remote += buildFormatStr(FG_RED, BG_DEFAULT) + '%s%s%s' % (behind, symbols['diverged'], ahead)
+            #remote += buildFormatStr(FG_BLACK, BG_RED) + '%s%s%s' % (behind, symbols['diverged'], ahead)
         else:
             if behind:
-                remote += '%s%s' % (symbols['behind'], behind)
+                remote += buildFormatStr(FG_BLUE, BG_DEFAULT) + '%s%s' % (symbols['behind'], behind)
             if ahead:
-                remote += '%s%s' % (symbols['ahead of'], ahead)
+                remote += buildFormatStr(FG_YELLOW, BG_DEFAULT) + '%s%s' % (symbols['ahead of'], ahead)
 
+### BRANCH AND BRANCH STATUS ###
+gitStatus = ''
+if detached == True:
+    gitStatus = buildFormatStr(FG_BLACK, BG_RED) + str(branch)
+elif tracked == False:
+    gitStatus = buildFormatStr(FG_GREEN, BG_DEFAULT) + str(branch)
+else: # tracked == True and detac,hed == False
+    gitStatus = buildFormatStr(FG_RED, BG_DEFAULT) + str(branch)
+    #branchFormatted = buildFormatStr(FG_BLACK, BG_RED) + str(branch)
+    if remote is not '':
+        gitStatus += RESET + '❙' + remote # TODO is remote ever anything for untracked or no branch?
+gitStatus += RESET + '❙'
 
+### ADDED ### green
+if nb_staged != 0:
+    gitStatus += buildFormatStr(FG_GREEN, BG_DEFAULT) + '✚' + str(nb_staged) + RESET + '❙'
 
-out = '\n'.join([
-    str(branch),
-    remote,
-    staged,
-    conflicts,
-    changed,
-    untracked,
-    clean])
-print(out)
+### MERGE CONFLICTS ###
+if nb_U != 0:
+    gitStatus += buildFormatStr(FG_RED, BG_DEFAULT) + '✘' + str(nb_U) + RESET + '❙'
 
+### MODIFIED FILES ### red
+if nb_changed != 0:
+    gitStatus += buildFormatStr(FG_RED, BG_DEFAULT) + '✱' + str(nb_changed) + RESET + '❙'
+
+### UNTRACKED ###
+if nb_untracked != 0:
+    gitStatus += buildFormatStr(FG_BLUE, BG_DEFAULT) + '⚡' + str(nb_untracked) + RESET + '❙'
+
+### CLEAN REPO ###
+if clean:
+    gitStatus += buildFormatStr(FG_GREEN, BG_DEFAULT) + '✔' + RESET  + '❙' # make bold
+
+### STASHES ###
+# TODO add number of stahes
+
+print(gitStatus)
